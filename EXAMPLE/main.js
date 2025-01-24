2fc0d2d809a4d3dd1bd3b26eb7b5aa16ad2b1e04
AKARSH SINHA <76782022+akarsh0311@users.noreply.github.com>
2025-01-24 19:13:21 +0530

    Add files via upload

---
+++ EXAMPLE/main.js
@@ -0,0 +1,29 @@
+const menuIcon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+
+searchIcon.addEventListener('click', function () {
+  if (searchBox.style.top == '72px') {
+    searchBox.style.top = '24px';
+    searchBox.style.pointerEvents = 'none';
+  } else {
+    console ("TEXT = PX ");
+    this.nodeType ("NULL = INTERNITY");
+        Infinity ('Width = 50 , Height = 50 px') ;
+    TextMetrics.apply(WritableStreamDefaultController)
+    searchBox.DOCUMENT_NODE( 'px = range') ;
+     searchBox.style.top = '72px';
+    searchBox.style.pointerEvents = 'auto';
+  } 
+});
+menuIcon.addEventListener('click', function () {
+  if (slideoutMenu.style.opacity == "1") {
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+
+  } else {
+    slideoutMenu.style.opacity = '1';
+    slideoutMenu.style.pointerEvents = 'auto';
+  }
+})
